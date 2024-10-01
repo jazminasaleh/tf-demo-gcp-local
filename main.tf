30d36bc6b9654c348859c2a45a631f2efc74c9d1
jazminasaleh <jazminasaleh@gmail.com>
2024-10-01 09:09:49 -0500

    Actualizacion, creación de vista

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
-resource "google_bigquery_table" "test_terraform_example" {
-  dataset_id = "dataset_test"  
-  table_id   = "test_tf_github_consola"    
-  project    = "arcane-boulder-310022" 
+# Crear un dataset en BigQuery
+resource "google_bigquery_dataset" "dataset_test" {
+  dataset_id = "tf_dataset"  # ID del dataset
+  location   = "US"             # Ubicación del dataset
+  project    = "arcane-boulder-310022"  # ID del proyecto
+}
 
-   # Esquema de la tabla
+# Crear una tabla en el dataset
+resource "google_bigquery_table" "test_terraform_example_local" {
+  dataset_id = google_bigquery_dataset.dataset_test.dataset_id  # Referencia al dataset creado
+  table_id   = "test_tf_github_local"                         # ID de la tabla
+  project    = "arcane-boulder-310022"                           # ID del proyecto
+
+  # Esquema de la tabla
   schema = jsonencode([
     {
       "name" : "Nombres",
@@ -11,7 +19,7 @@ resource "google_bigquery_table" "test_terraform_example" {
       "mode" : "REQUIRED"
     },
     {
-      "name" : "Salario",
+      "name" : "Edad",
       "type" : "INTEGER",
       "mode" : "NULLABLE"
     },
@@ -21,4 +29,14 @@ resource "google_bigquery_table" "test_terraform_example" {
       "mode" : "NULLABLE"
     }
   ])
+}
+
+# Crear una vista
+resource "google_bigquery_table" "view_test" {
+  dataset_id = google_bigquery_dataset.dataset_test.dataset_id
+  table_id   = "view_test_table"
+  view {
+    query = "SELECT Nombres, Edad FROM `${google_bigquery_dataset.dataset_test.dataset_id}.test_tf_github_local`"
+    use_legacy_sql = false
+  }
 }
